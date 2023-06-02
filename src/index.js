0ca377fda536bcd24623310a96f85228f35e6861
jve0902 <jay090218@gmail.com>
2023-06-02 19:13:55 +0300

    defined metamask hook

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { MetaMaskProvider } from "metamask-react";
 import { ThemeProvider } from "./context/themeContext";
 
 
 ReactDOM.render(
   <>
     <React.StrictMode>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
+      <MetaMaskProvider>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </MetaMaskProvider>
     </React.StrictMode>
 
   </>,
